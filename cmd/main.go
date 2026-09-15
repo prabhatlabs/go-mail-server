77d81b997475dabc6fdb688c42ce825a454d11ff
Prabhat <workforprabhat1254@gmail.com>
2026-09-15 20:43:22 +0530

    refactor: restructure packages, add pgx driver and db schema/queries

--- cmd/main.go
+++ cmd/main.go
@@ -3,16 +3,16 @@ package main
 import (
 	"log"
 
-	"github.com/prabhatlabs/internals/lib"
-	"github.com/prabhatlabs/internals/services/mail"
+	"github.com/prabhatlabs/internals/lib/env"
+	"github.com/prabhatlabs/internals/lib/mail"
 )
 
 func main() {
-	err := lib.LoadEnv()
+	err := env.LoadEnv()
 	if err != nil {
 		log.Fatalln("Env load error: ", err)
 	}
 
-	recipient := "workforprabhat1254@gmail.com"
+	recipient := "prabhat@gluckglobal.com"
 	mail.SendMail(recipient, "Test Mail", "This is the body for the test mail, hope this works! <a href='https://x.com/prabhatlabs'>click here!</a>")
 }
